8bdfda91122fac7508185ffaafd8eabf03bba861
feras272 <ferasdev@gmail.com>
2020-05-04 02:57:37 +0300

    I finished the whole coderswag app finally with landscape orientation option added

--- app/src/main/java/com/example/coderswag/Adapters/ProductsAdapter.kt
+++ app/src/main/java/com/example/coderswag/Adapters/ProductsAdapter.kt
@@ -1,4 +1,44 @@
 package com.example.coderswag.Adapters
 
-class ProductsAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.coderswag.Model.Product
+import com.example.coderswag.R
+
+class ProductsAdapter(val product:List<Product>, val context:Context):RecyclerView.Adapter<ProductsAdapter.ProductHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.product_list_item, parent, false)
+        return ProductHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return product.size
+    }
+
+    override fun onBindViewHolder(holder: ProductHolder, position: Int) {
+        holder.bindProduct(product[position],context)
+    }
+
+    inner class ProductHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val productImage = itemView.findViewById<ImageView>(R.id.iv_product)
+        val productPrice = itemView.findViewById<TextView>(R.id.price_product)
+        val productText = itemView.findViewById<TextView>(R.id.tx_product)
+
+        fun bindProduct (product:Product,context: Context){
+            val resourceId = context.resources.getIdentifier(product.image,"drawable",context.packageName)
+            productImage.setImageResource(resourceId)
+            productText.text = product.title
+            productPrice.text = product.price
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/coderswag/Controller/Products.kt
+++ app/src/main/java/com/example/coderswag/Controller/Products.kt
@@ -1,16 +1,39 @@
 package com.example.coderswag.Controller
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.coderswag.Adapters.ProductsAdapter
 import com.example.coderswag.R
+import com.example.coderswag.Servieces.DataService
 import com.example.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class Products : AppCompatActivity() {
+
+    lateinit var adater:ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+        adater = ProductsAdapter(DataService.getProducts(categoryType),this)
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this,spanCount)
+        productsListID.layoutManager = layoutManager
+        productsListID.adapter = adater
+
+
+
+
     }
 }
